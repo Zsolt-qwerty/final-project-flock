9aa8e1cc21b25b030131a14a171e79395831f09d
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2025-03-04 10:39:49 +0000

    Test that will always pass

---
+++ src/app/example.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "vitest";
+
+describe("Test will always pass", () => {
+  test("1 + 2 should equal 3", () => {
+    const result = 3;
+    const expected = 1 + 2;
+    expect(expected).toBe(result);
+  });
+});
