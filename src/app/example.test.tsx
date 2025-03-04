dd8360a299282549783f1388bb88747bf9d2b06c
DamonSeahash5 <oliverhopwood5@gmail.com>
2025-03-04 11:13:14 +0000

    disabled rulest

--- src/app/example.test.tsx
+++ src/app/example.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, test } from "vitest";
 
 describe("Test will always pass", () => {
   test("1 + 2 should equal 3", () => {
-    const result = 3;
+    const result = 4;
     const expected = 1 + 2;
     expect(expected).toBe(result);
   });
