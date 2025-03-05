b0196eea2d8d90b9cb2cea0fd28f9cce3717d7d5
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-03-05 08:12:33 +0000

    style- Added font files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import styles from "./page.module.css";
-import HubContainer from "../app/components/HubContainer/HubContainer"
+import HubContainer from "../app/components/HubContainer/HubContainer";
 import SearchBar from "./components/SearchBar/SearchBar";
 import Navbar from "./components/NavBar/NarBar";
-// This component serves as the main page for the root route (/). 
+// This component serves as the main page for the root route (/).
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Navbar />
-       <SearchBar />
+      <SearchBar />
       <main className={styles.main}>
         <HubContainer />
       </main>
     </div>
   );
-};
+}
