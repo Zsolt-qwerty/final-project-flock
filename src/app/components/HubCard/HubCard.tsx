9a0c609d298c88d2ea3614da15604b9567338588
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-03-17 16:46:11 +0000

    error fix

--- src/app/components/HubCard/HubCard.tsx
+++ src/app/components/HubCard/HubCard.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 
 //Link from Next.js to make the button a link to the SingleHub page.
 //href prop is passed to the button component, so each button can link to a different URL.
